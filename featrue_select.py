804f4a3015d2631787b1d4747759c377cd637b78
jianzhiqi <675279854@qq.com>
2020-02-25 21:12:57 +0800

    add file

--- featrue_select.py
+++ featrue_select.py
@@ -2,21 +2,37 @@ import random
 import os
 import os.path
 import numpy as np
-from sklearn import ensemble
-from util import DataPre as dp
-
 from numpy import array
 from scipy.sparse import csr_matrix
+
+from sklearn import ensemble
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.model_selection import ShuffleSplit
+from sklearn.metrics import r2_score
+from collections import defaultdict
+
+from util import DataPre as dp
 grd = ensemble.RandomForestClassifier(n_estimators=30)
 training_file = 'train.txt'
 traindata_dir = './INSPEC_Data/INSPEC_train'
 valdata_dir = './INSPEC_Data/INSPEC_validation'
+train = []
+label = []
 tf_name = None
 
-def check_data(data, tag, qid, type):
+
+
+def write_file(data, tag, qid, type):
     for item in data:
-        if len(item) > 3 and item[0] and item[1] and item[2]:
+        if len(item) >= 3 and isinstance(item[0],list) and isinstance(item[1],list) and isinstance(item[2],list):
+            print(item[0])
+            print(item[1])
+            print(item[2])
+
             pt = item[0] + item[1] + item[2]
+            train.append(pt)
+            label.append(tag)
+
             with open('./' + str(type) + '.txt', 'a') as f:
                 c = 1
                 f.write(str(tag) + ' ' + 'qid:' + str(qid) + ' ')
@@ -32,7 +48,8 @@ def file_name(file_dir):
         # print(dirs)  # 当前路径下所有子目录
         # print(files)  # 当前路径下所有非目录子文件
         return(files)
-tf_name = os.listdir(os.path.join(traindata_dir))
+# tf_name = os.listdir(os.path.join(traindata_dir))
+tf_name = ['VpWAaLm_jQU999p9zWB.xlsx']
 # data = dp('./INSPEC_Data/INSPEC_train/Vp9AaLm_jQ9Q0U90W1S.xlsx').load()
 
 for item in tf_name:
@@ -47,8 +64,8 @@ for item in tf_name:
                 pos_train = random.sample(data[0], neo_len)
                 neo_train = random.sample(data[1], round(neo_len * 0.7))
                 qid = round(random.random() * 10000)
-                check_data(neo_train, 1, qid, type)
-                check_data(pos_train, 0, qid, type)
+                write_file(neo_train, 1, qid, type)
+                write_file(pos_train, 0, qid, type)
 
 
 
@@ -59,5 +76,24 @@ for item in tf_name:
 # b = [1]*len(neo_train)
 
 # train = np.hstack([array()])
-# grd.fit(array(neo_train+pos_train),array(b+a))
-# print(grd.feature_importances_)
+t = array(train)
+t[np.isnan(t)] = 0
+grd.fit(t,array(label))
+print(grd.feature_importances_)
+
+rf = RandomForestRegressor()
+scores = defaultdict(list)
+
+# crossvalidate the scores on a number of different random splits of the data
+for train_idx, test_idx in ShuffleSplit(len(X), 100, .3):
+    X_train, X_test = train[train_idx], train[test_idx]
+    Y_train, Y_test = label[train_idx], label[test_idx]
+    r = rf.fit(X_train, Y_train)
+    acc = r2_score(Y_test, rf.predict(X_test))
+    for i in range(train.shape[1]):
+        X_t = X_test.copy()
+        np.random.shuffle(X_t[:, i])
+        shuff_acc = r2_score(Y_test, rf.predict(X_t))
+        scores[names[i]].append((acc - shuff_acc) / acc)
+print("Features sorted by their score:")
+print(sorted([(round(np.mean(score), 4), feat) for feat, score in scores.items()], reverse=True))
--- util/load.py
+++ util/load.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 # df = pd.read_excel('./Vp9AaLm_jQ9Q0U90W1S.xlsx',None)
 # df = pd.read_excel('./Vp3AaLm_jQU9Uzpzp0u.xlsx',None)
 # print('df: \n',df)
@@ -17,12 +18,41 @@ class DataPre(object):
         tag = -1
         c = -1
         record = -1
+
+        prev = None
+        prev_s = None
+        k = 0
+        d = 0
+        cur = [0]
+        ma = [0]
+
         result_value = []
-        phase_value = [None]
+        phase_value = [np.nan]
         train = []
         neotrain = []
         # df = pd.read_excel('./Vp9AaLm_jQ9Q0U90W1S.xlsx', None)
         df = pd.read_excel(self.file, None)
+
+
+        for s in df['Parameters_Table'].iloc[:, 8]:
+            if s != 0 and s > (len(cur) - 1):
+                cur.extend([0] * (s - len(cur) + 1))
+                ma.extend([0] * (s - len(ma) + 1))
+            if s == prev and df['Parameters_Table'].iloc[d, 0] == prev_s:
+                cur[s] += 1
+            else:
+                prev = s
+                prev_s = df['Parameters_Table'].iloc[d, 0]
+                cur[s] = 1
+            ma[s] = max((cur[s], ma[s]))
+            # if cur[s] > ma[s]:
+            #     ma[s] = cur[s]
+            #     print(str(d)+ ':' +str(s) + ':' + str(ma[s]))
+            d += 1
+
+        print("test:" + str(len(ma)) + str(ma))
+
+
         for process_c, item in enumerate(df['Process_Table'].iloc[:, 0]):
             for process in df['Phase_Table'].iloc[phase_c:, 0]:
                 if process == item:
@@ -35,27 +65,33 @@ class DataPre(object):
                         # for param_c, i in enumerate(df['Parameters_Table'].iloc[:,0]):
                         for i in df['Parameters_Table'].iloc[val_c:, 0]:
                             if i == j:
-                                result_value.append(df['Parameters_Table'].iloc[val_c, 2])
                                 tag = df['Parameters_Table'].iloc[val_c, 8]
+                                if len(result_value) == 0:
+                                    result_value = [np.nan] * ma[tag]
+                                    k = 0
+                                result_value[k] = df['Parameters_Table'].iloc[val_c, 2]
+                                k += 1
+                                # result_value.append(df['Parameters_Table'].iloc[val_c, 2])
                                 val_c += 1
                                 c = 1
                             if c == 1 and i != j:
                                 break
                         if tag != 0 and tag > len(phase_value) - 1:
-                            phase_value.extend([None] * (tag - len(phase_value) + 1))
+                            phase_value.extend([np.nan] * (tag - len(phase_value) + 1))
                         if tag != -1:
                             phase_value[tag] = result_value
                         c = -1
+                        tag = -1
                         result_value = []
                     # print(item+str(phase_value))
                     if df['Process_Table'].iloc[process_c, 1] == 2:
                         neotrain.append(phase_value)
-                        print(item + str(phase_value))
+                        print(item + ':' + str(len(phase_value)) +str(phase_value))
                         # print('phase_id:'+str(phase_id))
                     else:
                         train.append(phase_value)
                     phase_id = []
-                    phase_value = [None]
+                    phase_value = [np.nan]
                     record = -1
                     break
         print(neotrain)
@@ -64,3 +100,5 @@ class DataPre(object):
             print(len(neotrain[0]))
         return((train,neotrain))
 
+# DataPre('../INSPEC_Data/INSPEC_train/VWpAaLm_jQU9pUW9pQO.xlsx').load()
+# DataPre('../INSPEC_Data/INSPEC_train/VpWAaLm_jQU999p9zWB.xlsx').load()
